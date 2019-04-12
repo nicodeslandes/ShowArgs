4dba7819167f85f1a0ccca04bdc442f07184f422
Nicolas Deslandes <ndeslandes@live.fr>
2019-04-12 15:39:11 +0100

    Initial version

---
+++ ShowArgs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ShowArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("<no arguments>");
+                return;
+            }
+
+            int numberOfDigits(int value)
+            {
+                if (value < 0) throw new Exception("Nope, not doing it");
+
+                var result = 1;
+                while (value >= 10)
+                {
+                    result++;
+                    value /= 10;
+                }
+
+                return result;
+            }
+
+            var alignment = numberOfDigits(args.Length - 1);
+            var formatString = $"Arg {{0,{alignment}}}: {{1}}";
+            for (var i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(formatString, i, args[i]);
+            }
+        }
+    }
+}
